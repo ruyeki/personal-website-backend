9c7b16ef94cfd5cbe9decb0d130ddfd006024dd4
Ryan <ryan@persist-ai.com>
2025-11-02 19:11:08 -0800

    added memory and sessions for users

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session
 from flask_cors import CORS
 from openai import OpenAI
 import os
@@ -14,13 +14,17 @@ app = Flask(__name__)
 
 CORS(app)
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+app.secret_key = os.getenv("APP_SECRET_KEY")
 
 @app.route('/chat', methods = ['POST'])
 def chat():
     user_query = request.get_json()
     data = user_query['inputMessage']
-    print(user_query)
-    print(data)
+
+    if "chat_history" not in session:
+        session["chat_history"] = []
+
+    session["chat_history"].append(f"user question: {data}")
 
     with open('about.txt', 'r') as f:
         about_me = f.read()
@@ -30,16 +34,24 @@ def chat():
     Answer clearly, like you're explaining it to a friend.
     Don't say things like "based on the context" or "according to the text".
     Don't use bold or markdown formatting.
-    Don't always start with 'Oh'
+    Don't always start with 'Oh'.
+    Use the context, the user question, and the chat history to answer accordingly. 
 
     Context:
     {about_me}
 
     Question:
     {data}
+
+    Chat History: 
+    {session["chat_history"]}
+
     """
     model = genai.GenerativeModel("gemini-2.5-pro")
     response = model.generate_content(prompt)
+    session["chat_history"].append(f"bot response: {response.text}")
+    # Save session
+    session.modified = True
 
     text_response = {"data": response.text}
 
