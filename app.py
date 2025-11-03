75146cf0c7e8f41a9db989991f4f32b3568e4968
Ryan <ryan@persist-ai.com>
2025-11-02 17:51:08 -0800

    made a simple chat app backend

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
+
+load_dotenv()
+
+app = Flask(__name__)
+CORS(app)
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+@app.route('/chat', methods = ['POST'])
+def chat():
+    user_query = request.get_json()
+    data = user_query['inputMessage']
+    print(user_query)
+    print(data)
+
+    with open('about.txt', 'r') as f:
+        about_me = f.read()
+
+    prompt = f"""
+    You're a friendly and natural-sounding person having a casual conversation.
+    Answer clearly, like you're explaining it to a friend.
+    Don't say things like "based on the context" or "according to the text".
+    Don't use bold or markdown formatting.
+    Don't always start with 'Oh'
+
+    Context:
+    {about_me}
+
+    Question:
+    {data}
+    """
+    model = genai.GenerativeModel("gemini-2.5-pro")
+    response = model.generate_content(prompt)
+
+    text_response = {"data": response.text}
+
+    return jsonify(text_response)
+    
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
