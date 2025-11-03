dc2feab0e3aa4a638fc69a47fa596956409ca688
Ryan <ryan@persist-ai.com>
2025-11-02 18:05:25 -0800

    fixed port issue

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from openai import OpenAI
 import os
 from dotenv import load_dotenv
 import google.generativeai as genai
+import os
+
+port = int(os.environ.get("PORT", 10000))
 
 load_dotenv()
 
@@ -45,4 +48,4 @@ def chat():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=port)
